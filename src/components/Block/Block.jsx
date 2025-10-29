ef1329073b0616b91ebceda272156ed364be1d1b
Vladimir Kolesnikov <9858713@gmail.com>
2025-10-29 12:42:20 +0200

    init commit

---
+++ src/components/Block/Block.jsx
@@ -0,0 +1,7 @@
+import './Block.scss';
+
+export const Block = () => {
+    return (
+        <div className="block">this is Block!</div>
+    )
+};
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,11 @@
+import './Container.scss';
+import { Block } from '../Block';
+
+export const Container = () => {
+    return (
+        <div className="container">
+            <h1>this is container!</h1>
+            <Block />
+        </div>
+    )
+};
